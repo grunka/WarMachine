b1826589cfe4fa2787a8abc82a5b9815925854df
John Wästerlund <grunka@Grunka.local>
2012-03-05 22:37:08 +0100

    Adding thread count configuration

--- src/main/java/se/grunka/basal/BasalConfig.java
+++ src/main/java/se/grunka/basal/BasalConfig.java
@@ -9,4 +9,5 @@ public class BasalConfig {
     public int port = 8080;
     public Map<String, String> paths = new HashMap<String, String>();
     public Level logLevel = Level.INFO;
+    public int threads = 100;
 }
